3e822b5da39c92740581945bb1380dbb3aaf4ece
PoojithaYadav <poojithyadav777@gmail.com>
2019-01-04 23:40:17 +0530

    updating

--- PythonBasics/Date-04_01_2019/Program7.py
+++ PythonBasics/Date-04_01_2019/Program7.py
@@ -1,20 +1,4 @@
-# def func(a, l=[]):
-#     l.append(a)
-#     return l
-#
-#
-# print(func(1))
-# print(func(2))
-# print(func(3))
-# print(func('a'))
-# print(func('b'))
-#
-#
-#
-# def func(l=[]):
-#     print(l)
-#
-# func()
+
 class DefultArg():
     def defaultArg(self,z, List1=[]):
         List1.append(z)
